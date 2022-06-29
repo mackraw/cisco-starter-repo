daaca8a4bb57c55e972dc13c0f3d117a2c7317cf
Mac Krawczyk <maciekrawczyk@gmail.com>
2022-06-29 10:52:56 -0500

    replaced dummy exhibits with ipv4 and ipv6

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,16 +1,19 @@
 import './App.css';
 import Banner from './components/Banner';
 import Exhibit from './components/Exhibit';
+import ShowIP from './components/ShowIP';
 
 function App() {
   return (
     <div className="App">
       <Banner title="Sextant" />
       <div className="grid">
-        <Exhibit name="Test Exhibit" />
-        <Exhibit name="Another Test" />
-        <Exhibit name="Third Test" />
-        <Exhibit name="Fourth Exhibit" />
+        <Exhibit name="Your IPv4 Address">
+          <ShowIP type="ipv4" />
+        </Exhibit>
+        <Exhibit name="Your IPv6 Address">
+          <ShowIP type="ipv6" />
+        </Exhibit>
       </div>
     </div>
   );
